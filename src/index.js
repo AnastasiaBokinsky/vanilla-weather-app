384a0eb5097cd78aad737214d799ae78362c4912
AnastasiaBokinsky <bokinskymail@gmail.com>
2022-08-31 22:02:48 -0400

    showing the current temperature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-
+function showTemperature(response) {
+  console.log(response);
+  let templeratureElement = document.querySelector("#temperature");
+  templeratureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("h1");
+  cityElement.innerHTML = response.data.name;
+  let descriptionelement = document.querySelector("#description");
+  descriptionelement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "3a65b7a336c78bcfe61bdbf873b77706";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemperature);
