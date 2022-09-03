4d1e4acd5b3606a01dfdc10bd5e26426e5037230
AnastasiaBokinsky <bokinskymail@gmail.com>
2022-09-03 13:52:04 -0400

    search engine added

--- src/index.js
+++ src/index.js
@@ -43,7 +43,18 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
   );
 }
-let apiKey = "3a65b7a336c78bcfe61bdbf873b77706";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(showTemperature);
+function search(city) {
+  let apiKey = "3a65b7a336c78bcfe61bdbf873b77706";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
