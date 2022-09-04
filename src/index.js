b970c53b2cf665e4c9d60240f5bf93723becd8c8
AnastasiaBokinsky <bokinskymail@gmail.com>
2022-09-03 20:40:51 -0400

    display the forecast data using API

--- src/index.js
+++ src/index.js
@@ -21,24 +21,36 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
 function showForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-      <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42" />
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img src="http://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png" alt="" width="42" />
       <span class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperatures-max">18°<span class="weather-forecast-temperatures-min">12°</span>
+        <span class="weather-forecast-temperatures-max">${Math.round(
+          forecastDay.temp.max
+        )}°<span class="weather-forecast-temperatures-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
         </span>
       </span>
     </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
